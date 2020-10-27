5f7480517dd9bc42540c54bd90c6393a39ac63a3
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-26 20:40:25 -0400

    removed unused styles

--- web/src/components/ContactForm.js
+++ web/src/components/ContactForm.js
@@ -1,14 +1,13 @@
 import React from "react";
-import styles from './ContactForm.modules.css'
+//import styles from './ContactForm.modules.css'
 
 const ContactForm = () => {
   return (
     <form
-      className={styles.root}
       action="https://formspree.io/f/xleobqbd"
       method="POST"
     >
-      <label className={styles.label}>
+      <label>
         Your email:
         <input type="text" id="_replyto" />
       </label>
