0ec8808174e326aba2b47124c61186ffbbe2a758
Hayden Young <haydenyounglex@ohtmail.com>
2020-10-27 12:18:34 -0400

    style and build contact form

--- web/src/components/ContactForm.js
+++ web/src/components/ContactForm.js
@@ -1,21 +1,21 @@
-import React from "react";
-//import styles from './ContactForm.modules.css'
+import React from 'react'
+import styles from './ContactForm.module.css'
+import {cn} from '../lib/helpers'
 
 const ContactForm = () => {
   return (
-    <form
-      action="https://formspree.io/f/xleobqbd"
-      method="POST"
-    >
-      <label>
-        Your email:
-        <input type="text" id="_replyto" />
-      </label>
-      <label>
-        Your message:
-        <textarea id="message" />
-      </label>
-      <button type="submit">Send</button>
+    <form className={styles.root} acceptCharset='UTF-8' action='https://formspree.io/f/xleobqbd' autoComplete='off' method='POST' target='_blank'>
+      <legend className={styles.headline}>Send me a message:</legend>
+      <label htmlFor='name'><span hidden>Name</span><br />
+        <input className={styles.feild} name='name' type='text' id='name' required placeholder='Your Name' />
+      </label><br />
+      <label htmlFor='email'><span hidden>Email</span><br />
+        <input className={styles.feild} name='email' type='email' id='email' required placeholder='Your Email' />
+      </label><br />
+      <label htmlFor='name'><span hidden>Message</span><br />
+        <textarea className={styles.feild} id='message' name='message' cols='30' rows='3' placeholder="What's up?" />
+      </label><br />
+      <button className={cn(styles.button, styles.feild)} type='submit' value='Submit'>Send</button>
     </form>
   )
 }
